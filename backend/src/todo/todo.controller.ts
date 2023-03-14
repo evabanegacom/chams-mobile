b1d5278dfa6e58c4ed9865e7858346889c4161b3
evabanegacom <udegbue69@gmail.com>
2023-03-14 11:54:26 +0100

    fixed edit functionality

--- backend/src/todo/todo.controller.ts
+++ backend/src/todo/todo.controller.ts
@@ -36,7 +36,9 @@ export class TodoController {
 
   @Put(':id')
 async update(@Param('id') id: any, @Body() todo: TodoEntity): Promise<TodoEntity> {
-  const existingTodo = await this.todoRepository.findOne(id);
+  const existingTodo = await this.todoRepository.findOne({
+    where: { id: id },
+  });
   if (!existingTodo) {
     console.log(`Todo with id ${id} not found`)
     throw new NotFoundException(`Todo with id ${id} not found`);
@@ -45,6 +47,7 @@ async update(@Param('id') id: any, @Body() todo: TodoEntity): Promise<TodoEntity
   await this.todoRepository.save(updatedTodo);
   return updatedTodo;
 }
+
 
   @Delete(':id')
   async delete(@Param('id') id: number): Promise<void> {
